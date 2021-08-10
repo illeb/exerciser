5eb8ab7d52b037833194a94ace32e7cdbd58ba63
Stefano Belli <>
2021-08-10 08:48:08 +0200

    sistemato parsing2

--- server/parsers/quiz-extractor.ts
+++ server/parsers/quiz-extractor.ts
@@ -31,26 +31,29 @@ const parseQuestions = async (bancaDatiId: number): Promise<Category[]> => {
       categoria: nodoLink.contents().first().text(),
       nDomande: +nodoLink.find('.eleque2').text().replace(/\(*(quesiti)*\)*/g, '').trim(),
       link: nodoLink.attr().href
-    }  
-  });
+    }
+  }).filter((categoria: any) => !categoria.link.includes('idc'));
 
-  categorie = categorie.slice(0,1);
-  const categorieDomande = await Promise.all(categorie.map(async ({categoria, link, nDomande}: any) => {
-    nDomande = 10;
+  const categorieDomande = await Promise.all(categorie.map(async ({categoria, link, nDomande}: any, index: number) => {
     const idCategoria = link.split('ida')[1];
     let domande: Quiz[] = [];
+    console.log(`categoria ${index + 1}/${categorie.length}\n\t`)
     // Questa costruzione è orribile, ma è l'unico modo per creare una catena operazioni scadenzate tra i 3 e 5 secondi
     for(let i = 0; i < nDomande; i++) {
-      const quizLink = `${baseURL}/maketest.asp?tif=2&max=${90}&ida=${idCategoria}&sx=0&ord=1&ini=${i+1}`;
+      const quizLink = `${baseURL}/maketest.asp?tif=2&ida=${idCategoria}&sx=0&ord=1&ini=${i+1}`;
       const $ = await get$(quizLink);
       const domanda = $('.tDomanda').text().replace('Cerca con Google...', '');
       const risposte = $('.tRisposta').toArray().map((value: any) => $(value).text().substring(1));
 
       let rispostaEsatta = $('body').html().split('\n').find((s: string | string[]) => s.indexOf('function rightAns(') != -1);
-      rispostaEsatta = +rispostaEsatta.split('"')[1].slice(0,1);
+      rispostaEsatta = +rispostaEsatta.split('"')[1].slice(0,1) - 1; // la risposta è in base uno, per questo -1
 
-      domande = domande.concat(new Quiz(null, domanda, risposte, risposte[rispostaEsatta]));;
-      await waitFor(getRandomArbitrary(3500, 5230))
+      domande = domande.concat(new Quiz(null, domanda, risposte, risposte[rispostaEsatta]));
+
+      process.stdout.clearLine(0);  // clear current text
+      process.stdout.cursorTo(0);  // move cursor to beginning of line
+      process.stdout.write(`${i + 1}/${nDomande},`);
+      await waitFor(getRandomArbitrary(900, 1500))
     }
 
     return new Category(null, categoria, domande);
