50de727962379953b8ba260fa053a0ad20108d6b
Stefano Belli <>
2021-11-14 16:22:55 +0100

    Some other progresses on the modal

--- client/src/app/shared/questionarrie-chooser/questionarrie-chooser.component.ts
+++ client/src/app/shared/questionarrie-chooser/questionarrie-chooser.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
-import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { MatDialogRef } from '@angular/material/dialog';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { AppState } from '@state/app.state';
 import { loadCategories } from '@state/quiz/quiz.actions';
 import { selectCategories } from '@state/quiz/quiz.selectors';
-import { BehaviorSubject } from 'rxjs';
+import { combineLatest, Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 import { Category } from 'src/app/model/category';
 import { QuizGroupRequest } from '../../categories/QuizGroupRequest';
 
@@ -20,17 +21,17 @@ import { QuizGroupRequest } from '../../categories/QuizGroupRequest';
        <mat-form-field class="w100p" appearance="fill">
         <mat-label>Categories</mat-label>
         <mat-chip-list #chipList>
-          <mat-chip *ngFor="let category of selectedCategories" [selectable]="true" [removable]="true" (removed)="removeCategory(category)">
+          <mat-chip *ngFor="let category of selectedCategories" [removable]="true" (removed)="removeCategory(category)">
             {{category.name}}
             <button matChipRemove>
               <mat-icon>cancel</mat-icon>
             </button>
           </mat-chip>
-        <input type="text" matInput [formControl]="categoriesCtrl" [matAutocomplete]="autocomplete" [matChipInputFor]="chipList">
+        <input type="text" #input matInput [formControl]="categoriesCtrl" [matAutocomplete]="autocomplete" [matChipInputFor]="chipList">
         </mat-chip-list>
         
         <mat-autocomplete #autocomplete="matAutocomplete" (optionSelected)="selected($event)">
-          <mat-option *ngFor="let category of categories$ | async" [value]="category">
+          <mat-option *ngFor="let category of filteredCategories | async" [value]="category">
             {{category.name}}
           </mat-option>
         </mat-autocomplete>
@@ -57,15 +58,20 @@ import { QuizGroupRequest } from '../../categories/QuizGroupRequest';
 })
 export class QuestionarrieChooserComponent implements OnInit {
   public dialogRef: MatDialogRef<QuestionarrieChooserComponent>;
+  
+  @ViewChild('input') input: ElementRef; 
 
-  numberQuestions: number = 15;
-  selectedCategories: Category[] = [];
-  isRandom: boolean = true;
-  categoriesCtrl = new FormControl();
-  categories$ = this.store.select(selectCategories);
+  public numberQuestions: number = 15;
+  public isRandom: boolean = true;
+  public categoriesCtrl = new FormControl();
+  public filteredCategories: Observable<Category[]>;
+  public selectedCategories: Category[] = [];
+  private categories$: Observable<Category[]>;
   constructor(dialogRef: MatDialogRef<QuestionarrieChooserComponent>, router: Router, { snapshot }: ActivatedRoute, private store: Store<AppState>) {
     this.dialogRef = dialogRef;
     this.store = store;
+    this.store.dispatch(loadCategories());
+    this.categories$ = store.select(selectCategories);
     
     this.dialogRef.beforeClosed().subscribe(() => {
       // Remove query params
@@ -77,18 +83,29 @@ export class QuestionarrieChooserComponent implements OnInit {
         queryParamsHandling: 'merge'
       })
     })
+
+    this.filteredCategories = combineLatest([
+      this.categories$, 
+      this.categoriesCtrl.valueChanges.pipe(startWith(''))
+    ]).pipe(
+      map(([categories, searchValue]: [Category[], string | Category]) => {
+        const name = typeof searchValue === 'string' ? searchValue : searchValue.name;
+        return categories.filter(category => !this.selectedCategories.includes(category) && category.name.toLowerCase().includes(name.toLowerCase()));
+      })
+    )
   }
 
-  ngOnInit(): void {
-    this.store.dispatch(loadCategories());
-  }
+  ngOnInit(): void { }
 
   selected(selected: MatAutocompleteSelectedEvent) {
-    this.selectedCategories = this.selectedCategories.concat(selected.option.value)
+    this.selectedCategories = this.selectedCategories.concat(selected.option.value);
+    this.categoriesCtrl.patchValue('')
+    this.categoriesCtrl.updateValueAndValidity();
+    this.input.nativeElement.value = '';
   }
 
   removeCategory(category) {
-
+    this.selectedCategories = this.selectedCategories.filter(currCategory => currCategory != category);
   }
 
   startQuiz() {
